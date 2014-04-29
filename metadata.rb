d9703b608a380c0cc11cc91eeb7a103b48cf0354
achannegowda <achannegowda@qwinixtech.com>
2014-04-29 17:35:59 +0530

    second commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'myvim'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures myvim'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'vim'
